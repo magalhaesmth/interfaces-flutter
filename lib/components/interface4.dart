9a591e622568ebe3822d0d59dd4a9fff19e8845c
magalhaesmth <matheusurbano2002@hotmail.com>
2022-09-20 00:26:14 -0300

    Atualizando

--- lib/components/interface4.dart
+++ lib/components/interface4.dart
@@ -14,20 +14,31 @@ class _Interface4 extends State<Interface4> {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       body: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(50),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: [
+              criarInput(
+                  'Digite seu nome', Icon(Icons.person), TextInputType.text),
+              criarInput('Digite seu e-mail', Icon(Icons.mail),
+                  TextInputType.emailAddress),
+              criarInput('Digite seu e-mail', Icon(Icons.password),
+                  TextInputType.visiblePassword),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget criarBotao(String descricao) {
+  Widget criarInput(String texto, Icon icone, TextInputType tipo) {
     return SizedBox(
-      child: TextField(),
+      child: TextField(
+        keyboardType: tipo,
+        decoration: InputDecoration(
+            border: InputBorder.none, icon: icone, hintText: texto),
+      ),
     );
   }
 }
